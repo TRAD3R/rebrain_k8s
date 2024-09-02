0ab0aaa974477366b454677e063b7c8086226f77
trad3r <c1mba@yandex.ru>
2024-09-02 08:52:00 +0300

    add printing

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/TRAD3R/tlog"
+import (
+	"time"
+
+	"github.com/TRAD3R/tlog"
+)
 
 func main() {
 	log := tlog.GetLogger(true)
 	log.Info("Starting...")
+
+	ch := make(chan int)
+
+	go print(log)
+	<-ch
+}
+
+func print(log *tlog.Logger) {
+	for {
+		time.Sleep(1 * time.Second)
+		log.Info("printing")
+	}
 }
